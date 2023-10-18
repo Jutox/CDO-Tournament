88922f9c2f0d7ff78b2c1b7af4d57471286078f8
juan muñoz <masterjutox@gmail.com>
2023-10-17 21:05:20 -0300

    primer commit creacion backend

---
+++ cdo_tournament_backend/src/main/java/com/example/cdo_tournament_backend/CdoTournamentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdo_tournament_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdoTournamentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdoTournamentBackendApplication.class, args);
+	}
+
+}
---
+++ cdo_tournament_backend/src/test/java/com/example/cdo_tournament_backend/CdoTournamentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdo_tournament_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdoTournamentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
