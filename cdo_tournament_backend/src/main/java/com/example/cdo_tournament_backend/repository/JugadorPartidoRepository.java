8874e2bf10e705341b36606d6bd430f65fb7b0b6
juan muñoz <masterjutox@gmail.com>
2023-10-20 22:09:00 -0300

    Repository BD creada

---
+++ cdo_tournament_backend/src/main/java/com/example/cdo_tournament_backend/repository/JugadorPartidoRepository.java
@@ -0,0 +1,14 @@
+package com.example.cdo_tournament_backend.repository;
+
+import com.example.cdo_tournament_backend.model.*;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface JugadorPartidoRepository extends JpaRepository<JugadorPartido, Integer>{
+
+    
+
+}
