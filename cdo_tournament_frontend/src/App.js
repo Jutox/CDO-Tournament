8925fbf9c3b6e3070d61ea9209390ddd76bca380
juan muñoz <masterjutox@gmail.com>
2023-11-04 20:05:32 -0300

    UI testing

--- cdo_tournament_frontend/src/App.js
+++ cdo_tournament_frontend/src/App.js
@@ -1,24 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import ListJugadoresComponent from './Components/ListJugadoresComponent';
+import NavBar from './Components/NavBar';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+          <div className='container'>
+            <Routes>
+              <Route exact path = '/' element = {<NavBar />}></Route>
+            </Routes>
+          </div>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
